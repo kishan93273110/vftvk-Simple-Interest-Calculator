e3719a2f4c62f20e1e217449e36070df67076a98
kishan93273110 <85736260+kishan93273110@users.noreply.github.com>
2021-06-12 11:33:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+
+    if (p <= 0) {
+        alert("Enter a positive Amount ")
+        document.getElementById("principal").focus();
+    } else {
+        var interest = (p * r * y) / 100;
+        document.getElementById("result").innerHTML = interest;
+        document.getElementById("result").style.backgroundColor = "yellow";
+    }
 }
-        
+
+function changeVal() {
+    var r = document.getElementById("rate").value;
+    document.getElementById("val").innerHTML = r + "%";
+}
